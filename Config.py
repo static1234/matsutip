99c2064b7d686907e9b2d06ce144d66a5f3c5340
FragingSquad <50500210+FragingSquad@users.noreply.github.com>
2019-05-17 22:48:07 +0200

    Add files via upload

---
+++ Config.py
@@ -0,0 +1,72 @@
+import keyboard
+import pyautogui
+import time
+print('Mettez le curseur sur les emplacements indiqués et appuyez sur q')
+time.sleep(1)
+print('Regarder un clip')
+while True:
+    try:
+        if keyboard.is_pressed('q'):
+            print(pyautogui.position())
+            break
+        else:
+            pass
+    except:
+        break
+time.sleep(1)
+print('Clip n°1 =')
+while True:
+    try:
+        if keyboard.is_pressed('q'):
+            print(pyautogui.position())
+            
+            break
+        else:
+            pass
+    except:
+        break
+time.sleep(1)
+print('Clip n°2 =')
+while True:
+    try:
+        if keyboard.is_pressed('q'):
+            print(pyautogui.position())
+            break
+        else:
+            pass
+    except:
+        break
+time.sleep(1)
+print('Clip n°3 =')
+while True:
+    try:
+        if keyboard.is_pressed('q'):
+            print(pyautogui.position())
+            break
+        else:
+            pass
+    except:
+        breakss
+time.sleep(1)
+print('Fermer la fenetre =')
+while True:
+    try:
+        if keyboard.is_pressed('q'):
+            print(pyautogui.position())
+            break
+        else:
+            pass
+    except:
+        break
+time.sleep(1)
+print('Fermer la fenetre merci =')
+while True:
+    try:
+        if keyboard.is_pressed('q'):
+            print(pyautogui.position())
+            break
+        else:
+            pass
+    except:
+        break
+time.sleep(1)
