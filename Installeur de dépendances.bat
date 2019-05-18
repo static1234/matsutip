ae618e67b32c5c9185a635f89f1af6b7955ff6cb
static1234 <46049806+static1234@users.noreply.github.com>
2019-05-18 14:19:12 +0200

    2 fois le mot install.

--- Installeur de dépendances.bat
+++ Installeur de dépendances.bat
@@ -1,5 +1,5 @@
 py -m pip install pyscreenshot
-py -m pip install install opencv-python
+py -m pip install opencv-python
 py -m pip install numpy
 py -m pip install pyautogui
 py -m pip install pynput
